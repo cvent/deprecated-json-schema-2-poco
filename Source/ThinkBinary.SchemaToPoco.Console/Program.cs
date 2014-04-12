cf73956028289a7798d893c536d3734471e4edb9
Pervez Choudhury <codedemonuk@gmail.com>
2014-04-12 18:27:42 +0100

    Added NLog for console logging

--- Source/ThinkBinary.SchemaToPoco.Console/Program.cs
+++ Source/ThinkBinary.SchemaToPoco.Console/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace ThinkBinary.SchemaToPoco.Console
 {
 	class Program
 	{
+		private static Logger _log;
+
 		static Int32 Main(string[] args)
 		{
+			try
+			{
+				ConfigureLoging();
 
-			return (int) ExitCodes.Ok;
+				return (int)ExitCodes.Ok;
+			}
+			catch (Exception e)
+			{
+				_log.Fatal(e);
+				return (int)ExitCodes.AbnormalExit;
+			}
+		}
+
+		private static void ConfigureLoging()
+		{
+			var coloredConsoleTarget = new ColoredConsoleTarget
+			{
+				Layout = "${longdate} | ${logger} | ${level} | ${message}"
+			};
+			var loggingRule =  new LoggingRule("*", LogLevel.Debug, coloredConsoleTarget);
+			LogManager.Configuration = new LoggingConfiguration();
+			LogManager.Configuration.AddTarget("Console", coloredConsoleTarget);
+			LogManager.Configuration.LoggingRules.Add(loggingRule);
+			LogManager.ReconfigExistingLoggers();
+
+			_log = LogManager.GetCurrentClassLogger();
 		}
 	}
 }

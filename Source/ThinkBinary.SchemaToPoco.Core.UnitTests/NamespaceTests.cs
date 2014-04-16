35064d1bff2ff6822b124cc5846f05ff41a000ce
Pervez Choudhury <codedemonuk@gmail.com>
2014-04-16 22:53:25 +0100

    Started to implement class to create code compile unit from schema

---
+++ Source/ThinkBinary.SchemaToPoco.Core.UnitTests/NamespaceTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace ThinkBinary.SchemaToPoco.Core.UnitTests
+{
+	[TestFixture]
+	public class NamespaceTests
+	{
+		[TestCase("SingleLevel")]
+		[TestCase("Two.Levels")]
+		public void Requested_name_space_is_used_for_generated_code(string requestedNamespace)
+		{
+			var sut = new JsonSchemaToCodeUnit(requestedNamespace);
+			var codeUnit = sut.Execute();
+
+			Assert.That(codeUnit.Namespaces.Count, Is.EqualTo(1));
+			Assert.That( codeUnit.Namespaces[0].Name, Is.EqualTo(requestedNamespace));
+		}
+
+		[Test]
+		public void Blank_namespace_is_used_for_generated_code_when_no_namespace_specified()
+		{
+			var sut = new JsonSchemaToCodeUnit();
+			var codeUnit = sut.Execute();
+
+			Assert.That(codeUnit.Namespaces.Count, Is.EqualTo(1));
+			Assert.That(codeUnit.Namespaces[0].Name, Is.EqualTo(""));
+		}
+	}
+}
---
+++ Source/ThinkBinary.SchemaToPoco.Core/JsonSchemaToCodeUnit.cs
@@ -0,0 +1,26 @@
+﻿using System.CodeDom;
+
+namespace ThinkBinary.SchemaToPoco.Core
+{
+	public class JsonSchemaToCodeUnit
+	{
+		private string _codeNamespace;
+
+		public JsonSchemaToCodeUnit(string requestedNamespace)
+		{
+			_codeNamespace = requestedNamespace;
+		}
+
+		public JsonSchemaToCodeUnit() : this("")
+		{
+		}
+
+		public CodeCompileUnit Execute()
+		{
+			var codeCompileUnit =  new CodeCompileUnit();
+			codeCompileUnit.Namespaces.Add(new CodeNamespace(_codeNamespace));
+
+			return codeCompileUnit;
+		}
+	}
+}

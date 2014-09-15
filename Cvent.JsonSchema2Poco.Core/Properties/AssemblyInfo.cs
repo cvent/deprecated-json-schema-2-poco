ef8d5421d729b142cbe599064a53eab134260c55
Roling, Luke <lroling@cvent.com>
2014-09-15 17:42:06 -0400

    Adding in missing files.

--- Cvent.JsonSchema2Poco.Core/Properties/AssemblyInfo.cs
+++ Cvent.JsonSchema2Poco.Core/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cvent.JsonSchema2Poco.Cor")]
+[assembly: AssemblyTitle("Cvent.JsonSchema2Poco.Core")]
 [assembly: AssemblyDescription("Generates C# classes from json schemas.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cvent.JsonSchema2Poco.Cor")]
+[assembly: AssemblyProduct("Cvent.JsonSchema2Poco.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
